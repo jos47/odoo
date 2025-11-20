f9cec6f93a01f20e3175392a6671fc9f0f91ca9e
jos47 <player2147@gmail.com>
2025-11-20 18:25:53 +0300

    functional contact types

--- addons/terpo_contact_types/__manifest__.py
+++ addons/terpo_contact_types/__manifest__.py
@@ -1,12 +1,13 @@
 {
     'name': 'Company Contact Relationship Types',
-    'version': '16.0.1.0.0',
+    'version': '18.0.1.0.0',
     'category': 'Contacts',
     'summary': 'Add relationship types to contacts',
     'description': 'Add many-to-many relationship types field to contacts',
     'depends': ['contacts'],
     'data': [
-        'data/relationship_type_data.xml',  # Добавляем данные справочника
+        'security/ir.model.access.csv',  # Добавь эту строку
+        'data/relationship_type_data.xml',
         'views/res_partner_views.xml',
     ],
     'installable': True,
